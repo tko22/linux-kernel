d7b736f2ffb0f1700b74ac0d3a91296b0055c01a
Tae <t.lawphong@gmail.com>
2018-03-08 16:40:27 -0600

    put printf("after init") to kernel.c

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -143,7 +143,7 @@ void entry(unsigned long magic, unsigned long addr) {
     i8259_init();
     init_idt();
     init_rtc();
-
+    printf("after init");
     /* Initialize devices, memory, filesystem, enable device interrupts on the
      * PIC, any other initialization stuff... */
 
