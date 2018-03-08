647318cd0de3e2f58376b8b3264b0e8e098274a8
Hubert Deng <hubertdeng123@gmail.com>
2018-03-07 20:39:49 -0600

    added some keyboard stuff

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,4 +1,21 @@
 #include "keyboard.h"
+#include "lib.h"
 
 
 // Hubert
+
+char getScanCode(){
+  char c = 0;
+  do{
+    if(inb(0x60) != c){
+      c = inb(0x60);
+      if(c > 0){
+        return c;
+      }
+    }
+  }while(1);
+}
+
+char getChar(){
+  return scancode[getScanCode() + 1];
+}
