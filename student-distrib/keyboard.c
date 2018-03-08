071d9bb833d45f8df8d49a0af8fd0e6686248ba1
Hubert Deng <hubertdeng123@gmail.com>
2018-03-07 21:59:51 -0600

    keyboard done maybe

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,8 +1,59 @@
 #include "keyboard.h"
 #include "lib.h"
 
-
+int capsLock = 0, shift = 0;
 // Hubert
+char keyboardLowerCase[88] =
+{
+  ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '0', '-', '=', ' ', ' ', 'q', 'w', 'e', 'r', 't',
+  'y', 'u', 'i', 'o', 'p', '[', ']', ' ', ' ', 'a',
+  's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'',
+  '`', ' ', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
+  ',', '.', '/', ' ', '*', ' ', ' ', ' ', '1', '2',
+  '3', '4', '5', '6', '7', '8', '9', '0', 'N', 'S',
+  '7', '8', '9', '-', '4', '5', '6', '+', '1', '2',
+  '3', '0', '.', '0', '0', '0', '1', '2'
+};
+
+char keyboardUpperCase[88] =
+{
+  ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '0', '-', '=', ' ', ' ', 'Q', 'W', 'E', 'R', 'T',
+  'Y', 'U', 'I', 'O', 'P', '[', ']', ' ', ' ', 'A',
+  'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'',
+  '`', ' ', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+  ',', '.', '/', ' ', '*', ' ', ' ', ' ', '1', '2',
+  '3', '4', '5', '6', '7', '8', '9', '0', 'N', 'S',
+  '7', '8', '9', '-', '4', '5', '6', '+', '1', '2',
+  '3', '0', '.', '0', '0', '0', '1', '2'
+};
+
+char keyboardShiftLowerCase[88] =
+{
+  ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(',
+  ')', '_', '+', ' ', ' ', 'q', 'w', 'e', 'r', 't',
+  'y', 'u', 'i', 'o', 'p', '{', '}', ' ', ' ', 'a',
+  's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '\"',
+  '`', ' ', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
+  '<', '>', '?', ' ', '*', ' ', ' ', ' ', '1', '2',
+  '3', '4', '5', '6', '7', '8', '9', '0', 'N', 'S',
+  '7', '8', '9', '-', '4', '5', '6', '+', '1', '2',
+  '3', '0', '.', '0', '0', '0', '1', '2'
+};
+
+char keyboardShiftUpperCase[88] =
+{
+  ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(',
+  ')', '_', '+', ' ', ' ', 'Q', 'W', 'E', 'R', 'T',
+  'Y', 'U', 'I', 'O', 'P', '{', '}', ' ', ' ', 'A',
+  'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\"',
+  '`', ' ', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+  '<', '>', '?', ' ', '*', ' ', ' ', ' ', '1', '2',
+  '3', '4', '5', '6', '7', '8', '9', '0', 'N', 'S',
+  '7', '8', '9', '-', '4', '5', '6', '+', '1', '2',
+  '3', '0', '.', '0', '0', '0', '1', '2'
+};
 
 char getScanCode(){
   char c = 0;
@@ -17,9 +68,31 @@ char getScanCode(){
 }
 
 char getChar(){
-  return scancode[getScanCode() + 1];
-}
-
-void init_keyboard(){ // do this hubert
-
+  if(getScanCode() - 1 == 0x2A || getScaneCode() - 1 == 0x36){
+    shift = 1;
+  }
+  if(getScanCode() - 1 == 0xAA || getScaneCode() - 1 == 0xB6){
+    shift = 0;
+  }
+  if(getScanCode() - 1 == 0x3A && capsLock == 0){
+    capsLock = 1;
+  }
+  if(getScanCode() - 1 == 0x3A && capsLock == 1){
+    capsLock = 0;
+  }
+  if(getScanCode() < 88){
+    if(shift == 1 && capsLock == 0){
+      return keyboardShiftUpperCase[getScanCode() - 1];
+    }
+    else if(shift == 0 && capsLock == 0){
+      return keyboardLowerCase[getScanCode() - 1];
+    }
+    else if(shift == 1 && capsLock == 1){
+      return keyboardShiftLowerCase[getScanCode() - 1];
+    }
+    else if(shift == 0 && capsLock == 1){
+      return keyboardUpperCase[getScanCode() - 1];
+    }
+  }
+  return '';
 }
