18f19374fc0590c32f25f834d9b2a22e5a9ab929
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-08 16:21:31 -0600

    added idt handlers to header file

--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -2,7 +2,25 @@
 #define _IDT_H
 
 #define NUM_RESERVED       32
-void init_idt();
 
+void init_idt();
+void handle_debug_exception();
+void handle_nmi_interrupt();
+void handle_breakpoint();
+void handle_overflow();
+void handle_bound_range_exceeded();
+void handle_invalid_opcode();
+void handle_device_not_avail();
+void handle_double_fault();
+void handle_coprocessor_seg();
+void handle_invalid_tss();
+void handle_seg_not_present();
+void handle_stack_seg_fault();
+void handle_general_protection();
+void handle_page_fault();
+void handle_FPU_floating_point_error();
+void handle_alignment_check_exception();
+void handle_machine_check_exception();
+void handle_SIMD_floating_point_exception();
 
 #endif
