2b2931ba77b2e03c8b043c9bf45cd32028cc84e3
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-07 18:53:35 -0600

    completed idt exeception for loop

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -8,9 +8,46 @@ void init_idt(){
     // - From 48 to 255 : software interrupts
 
     // 32 Exceptions to handle
+    
     for(i = 0; i < 32; i++) {
-        idt[i].seg_selector
+        // For easy look up
+        // uint16_t offset_15_00;
+        // uint16_t seg_selector;
+        // uint8_t  reserved4;
+        // uint32_t reserved3 : 1;
+        // uint32_t reserved2 : 1;
+        // uint32_t reserved1 : 1;
+        // uint32_t size      : 1;
+        // uint32_t reserved0 : 1;
+        // uint32_t dpl       : 2;
+        // uint32_t present   : 1;
+        // uint16_t offset_31_16;
 
+        // i dont know whether these are all correct
+        // I couldn't find where the interrupt entry descriptors
+        // were described in the intel manual
+        // so I referenced this http://www.independent-software.com/writing-your-own-toy-operating-system-setting-up-the-interrupt-descriptor-table-idt/
+        // oh well, hopefully it's right
+
+        idt[i].seg_selector = KERNEL_CS; // not sure, either KERNEL_CS or KERNEL_DS
+        idt[i].reserved4 = 0;  // its 0 because it says its zero - unused
+
+        // The next 2 bytes are type/attribute
+        // reserved 1-3 are gate types - I'm pretty sure that 
+        // exceptions use the 32bit trap gate, which is 0xf
+        idt[i].reserved3 = 1;
+        idt[i].reserved2 = 1;
+        idt[i].reserved1 = 1; 
+
+        // must be set to 0 for interrupt gates but its not an interrupt gate!
+        // because it is an exception!
+        idt[i].size = 1;  
+        // apparently, bits 5-6 are for dpl, but only bit 6 is named dpl
+        // so I guess I'll put 0 done for reserved 0
+        idt[i].reserved0 = 0;
+        idt[i].dpl = 0;     // exception handlers must have dpl = 0 - for sure right
         
+        idt[i].present = 1; // empty descriptor slots have 0, else 1 - for sure right - normally 1
+
     }
 }
--- student-distrib/x86_desc.h
+++ student-distrib/x86_desc.h
@@ -152,13 +152,13 @@ typedef union idt_desc_t {
         uint16_t offset_15_00;
         uint16_t seg_selector;
         uint8_t  reserved4;
-        uint32_t reserved3 : 1;
-        uint32_t reserved2 : 1;
-        uint32_t reserved1 : 1;
+        uint32_t reserved3 : 1; // start of types/attributes
+        uint32_t reserved2 : 1; 
+        uint32_t reserved1 : 1; // reserved 1-3 are gate types
         uint32_t size      : 1;
         uint32_t reserved0 : 1;
-        uint32_t dpl       : 2;
-        uint32_t present   : 1;
+        uint32_t dpl       : 2; 
+        uint32_t present   : 1; //bit 7 - end of 2 types/attributes
         uint16_t offset_31_16;
     } __attribute__ ((packed));
 } idt_desc_t;
