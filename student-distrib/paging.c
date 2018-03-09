53069865ecafb09e26a518c2ecbf5c17f52cd788
Hubert Deng <hubertdeng123@gmail.com>
2018-03-09 16:42:17 -0600

    added paging init

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -2,13 +2,13 @@
 
 void init_pages(){
   asm volatile ("                                     \n\
-          movl page_directory, cr3                    \n\
-          movl cr0, %%eax                             \n\
-          or %%eax, $0x8                              \n\
+          movl page_directory, %%cr3                  \n\
+          movl %%cr0, %%eax                           \n\
+          orl %%eax, $0x8                             \n\
           movl %%eax, cr0                             \n\
           "
           :"a"(address), "e"(enable)
-          : 
+          :
           :"memory"
   );
 }
