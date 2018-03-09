03c39d8c55bdd5d9566b547b1927e8ffc137deec
Hubert Deng <hubertdeng123@gmail.com>
2018-03-09 17:01:39 -0600

    added comments for initialization

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,6 +1,9 @@
 #include "paging.h"
 
 void init_pages(){
+    // stores start of page directory table in cr3
+    // sets the page enable bit and protected mode enable bit to high in cr0
+    // enables 4 MB pages (PSE) by modifying cr4 
     asm volatile ("                                     \n\
           movl page_directory, %%cr3                  \n\
           movl %%cr0, %%eax                           \n\
@@ -22,9 +25,8 @@ void init_pages(){
 
 
 void fill_pages(){
-    int i;  
+    int i;
     for ( i = 0; i < PAGE_DIR_SIZE; i++){
-        page_directory[i].present  
+        page_directory[i].present
     }
 }
-
