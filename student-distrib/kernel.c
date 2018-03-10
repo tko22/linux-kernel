186bf3c789ea53991d4a8aa575db9ecb8c9959c0
Timothy Ko <tk2@illinois.edu>
2018-03-10 17:45:20 -0600

    kernel

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -153,11 +153,10 @@ void entry(unsigned long magic, unsigned long addr) {
     /* Do not enable the following until after you have set up your
      * IDT correctly otherwise QEMU will triple fault and simple close
      * without showing you any output */
-  /*  fill_pages();
-    init_pages();
+    //init_pages();
     fill_pages();
     set_cr3(page_directory);
-    printf("Enabling Interrupts\n");*/
+    /*printf("Enabling Interrupts\n");*/
     sti();
 
 #ifdef RUN_TESTS
