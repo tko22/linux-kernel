6d125dadf800094d3e97da69a0b777da9a19fb1b
Shubhit <dharnid2@illinois.edu>
2018-03-16 13:27:00 -0500

    Finished RTC

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -5,6 +5,9 @@
 #define cmos_addr 0x70
 #define cmos_data 0x71
 
+const volatile int interrupt_flag;
+interrupt_flag = 0;
+
 /* void init_rtc();
  * Inputs: none
  * Return Value: none
@@ -34,8 +37,7 @@ void handle_rtc_interrupt(){
   outb(0x0C,cmos_addr);
   inb(0x71);
   printf("rtc handled");
-
-
+  interrupt_flag = 1;
 
 }
 
@@ -43,6 +45,24 @@ void handle_rtc_interrupt(){
 int32_t open_rtc(const uint8_t* filename){
 
     return 0;
+
+}
+
+//close the RTC
+int32_t close_rtc(const uint8_t* filename){
+
+    return 0;
+
+}
+
+//read the frequency value of the RTC.
+int32_t read_rtc(int32_t fd, void* buf, int32_t nbytes){
+
+      while(interrupt_flag != 1){   //spinning unitl flags informs you that interrupt has occured
+
+      }
+      interrupt_flag = 0;           //set the flag back to 0
+      return 0;                     //return 0 snce interrupt has occured. 
 
 }
 
