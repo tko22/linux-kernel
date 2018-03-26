851df91cd263371f40b68edc72179dadb9f70cc2
Tae <t.lawphong@gmail.com>
2018-03-25 20:33:42 -0500

    file system start and end

--- student-distrib/fs.c
+++ student-distrib/fs.c
@@ -27,14 +27,7 @@ void init_fs(){
 
 
 }*/
-/* for easy reference
-typedef struct dentry_t {
-    char file_name[MAX_NAME_LENGTH];
-    uint32_t file_type;
-    uint32_t inode_num;
-    uint8_t reserved[NUM_DENTRY_RESERVED_BYTES]; // reserved 24 bytes
-} dentry_t;
-*/
+
 int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry){
   //find the index by name and then call read_dentry_by_index
   //for loop based on boot first block's # of directory entries
