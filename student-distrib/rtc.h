10ad74c90658533f05afe3d4f4655505ea6140da
Shubhit <dharnid2@illinois.edu>
2018-03-25 20:49:04 -0500

    Added RTC_test

--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -11,6 +11,7 @@ int32_t close_rtc(const uint8_t* filename);
 int32_t read_rtc(int32_t fd, void* buf, int32_t nbytes);
 int32_t write_rtc(int32_t fd, const void* buf, int32_t nbytes);
 
+
 // static struct file_ops_jump_table_t rtc = {
 //     open_rtc,
 //     close_rtc,
