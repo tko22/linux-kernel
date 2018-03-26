2b0fb1361ab7dba2c23b57ca4af3d346ff31a6a1
Timothy Ko <tk2@illinois.edu>
2018-03-25 20:37:15 -0500

    fixing rtc

--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -11,8 +11,6 @@ int32_t close_rtc(const uint8_t* filename);
 int32_t read_rtc(int32_t fd, void* buf, int32_t nbytes);
 int32_t write_rtc(int32_t fd, const void* buf, int32_t nbytes);
 
-volatile int i_flag = 0;
-
 // static struct file_ops_jump_table_t rtc = {
 //     open_rtc,
 //     close_rtc,
--- student-distrib/sys.c
+++ student-distrib/sys.c
@@ -5,7 +5,7 @@
 void halt(){
     asm volatile (".1: hlt; jmp .1;");
 }
-int32_t open (const uint8_t* filename){
+/*int32_t open (const uint8_t* filename){
 
 }
 int32_t read (int32_t fd, void* buf, int32_t nbytes){
@@ -17,3 +17,4 @@ int32_t write (int32_t fd, const void* buf, int32_t nbytes){
 int32_t close (int32_t fd){
 
 }
+*/
