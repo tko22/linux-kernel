1042fc163aa36f379c59988079dd823e24080920
Tae <t.lawphong@gmail.com>
2018-03-26 19:38:37 -0500

    completed file system test

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -132,18 +132,20 @@ void read_dentry_by_name_test(){
 }
 void read_data_test(uint32_t inode){
 	uint32_t offset=0;
-	uint8_t buffer[1920];
-	uint32_t length = 1920; // it's the entire screen height * width = 24*80
+	uint8_t buffer[19200];
+	uint32_t length = 19200; // it's the entire screen height * width = 24*80 (times ten to make sure it will fit)
 	int i=0;
-	for(i=0;i<1920;i++){
+	for(i=0;i<19200;i++){
 		buffer[i] = NULL;
 	}
 	int32_t a;
 	a = read_data(inode,offset,buffer,length);
-	for(i=0;i<1920;i++){
+	inode_t* thisinode = ((void*)boot_block + (inode + 1) * BLOCK_SIZE);
+	for(i=0;i<thisinode->length;i++){ //print out using put c
+		if(i>19200) break; //quit if it exceeds our buffer
 		putc(buffer[i]);// put to the screen
 	}
-//	keyboard_write(0,(char*)buffer, 1920); //print out stuff to the screen
+//	keyboard_write(0,(char*)buffer, 1920); //print out stuff to the screen using keyboard write function
 }
 void print_by_name(uint8_t* filename){
 	dentry_t testdentry;
@@ -162,7 +164,7 @@ void list_all_files(){
 		read_dentry_by_index(i,pointertest);
 		uint32_t inode_num = pointertest->inode_num;
 		inode_t* thisinode = ((void*)boot_block + (inode_num + 1) * BLOCK_SIZE); // pointer to the given inode index
-		printf("file_name:%*s, file_type:%*d, file_size:%d\n",pointertest->file_name,pointertest->file_type,thisinode->length);
+		printf("file_name:%s, file_type:%d, file_size:%d\n",pointertest->file_name,pointertest->file_type,thisinode->length);
 	}
 }
 
@@ -204,13 +206,13 @@ void launch_tests(){
 	uint32_t i=0;
 	uint8_t testfilename[17][33] = {".","sigtest","shell","grep","syserr","rtc","fish","counter","pingpong","cat",
 	"frame0.txt","verylargetextwithverylongname.tx","ls","testprint","created.txt","frame1.txt","hello"};
-	//break tests.c:201
-	for(i=0;i<17;i++){ //print out every file name
+	list_all_files(); //TEST 1 list out files
+	for(i=0;i<17;i++){ //TEST 2 print out every file name
 		clear();
 		printf("print by filename:%s",testfilename[i]); // BREAK HERE TO SHOW IF IT WORKS
 		print_by_name(testfilename[i]);
 	}
-	for(i=0;i<17;i++){ //print out every file name by inode index
+	for(i=0;i<17;i++){ //TEST 4print out every file name by inode index
 		dentry_t testdentry;
 		dentry_t* pointertest = &testdentry;
 		int32_t a;
@@ -220,7 +222,6 @@ void launch_tests(){
 		read_data_test(pointertest->inode_num);
 
 	}
-	//list_all_files(); //TEST 1 list out files
 	//print_by_name(testfilename[0]) //TEST 2 print out by filename
 	//read_data_test(1); // TEST 4 read and printfiles by inode index
 	//read_dentry_by_index_test();
