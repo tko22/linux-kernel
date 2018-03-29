04da87ef9430cc8858f5a85f8fc9a1d3d6d28f0c
Tae <t.lawphong@gmail.com>
2018-03-28 19:59:57 -0500

    printf debug message in sys.c and move file to tests_cp1cp2.c

--- student-distrib/sys.c
+++ student-distrib/sys.c
@@ -12,25 +12,28 @@ void halt(){
 // }
 
 int32_t execute(const uint8_t* command){
+    printf("execute systemcall called\n");
     return 1;
 }
 int32_t read (int32_t fd, void* buf, int32_t nbytes){
     // returns number of bytes read
+    printf("read systemcall called\n");
     return 0; // returns 0 if initial file position is at or beyond EOF for normal files
 }
 int32_t write (int32_t fd, const void* buf, int32_t nbytes){
     // returns number of bytes written
+    printf("write systemcall called");
     return -1; // returns -1 on failure
 }
 int32_t open (const uint8_t* filename){
     // probably calls read_dentry_by_name
     // returns fd
+    printf("open systemcall called");
     return -1; // returns -1 on failure
 }
 int32_t close (int32_t fd){
     // returns 0 on success
-
-
+    printf("close systemcall called");
     return -1; // returns -1 on failure
 }
 
