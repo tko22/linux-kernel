dfe42b335163491a6d0bbad022d210354373375d
Tae <t.lawphong@gmail.com>
2018-04-06 14:46:12 -0500

    solve merge conflict in sys.h

--- student-distrib/sys.h
+++ student-distrib/sys.h
@@ -4,15 +4,14 @@
 #include "types.h"
 #include "file_desc.h"
 
-<<<<<<< HEAD
+
 #define USER_ADDRESS 0x8048000
 #define FOUR_KB 4096
-=======
+
 #define KERNEL_STACK_SIZE 0x2000 // 8KB
 #define MB8_ADDRESS 0x80000 // 8 MB
 #define KB8 0x2000 // 8 KB
 
->>>>>>> 8dc1d2de030758313a8076c6472d2a43819201d4
 
 extern void halt(); // TODO: REMOVE THIS and change to below
 // extern int32_t halt(uint8_t status);
