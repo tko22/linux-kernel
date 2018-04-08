2651a75c69ad9e376cc57106da59a068f720f4d0
Shubhit <dharnid2@illinois.edu>
2018-04-08 17:39:50 -0500

    Halt

--- student-distrib/sys.c
+++ student-distrib/sys.c
@@ -138,22 +138,32 @@ int32_t read (int32_t fd, void* buf, int32_t nbytes){
     // returns number of bytes read
     printf("read systemcall called\n");
     // TODO: get current pcb
+    pcb_t * caller_pcb;
+    caller_pcb = get_last_pcb();
     // then, check if file is in use or whether fd is in bounds
-    // if (fd >= 0 && fd < 8 && file_array[fd].flags == 1){
-    //     // TODO: Switch file_array to whatever is used in pcb
-    //     int32_t ret = file_array[fd]->file_op_table_pointer->read(file_array[fd], buf, nbytes);
-    //     return ret;
-    // }
+    if (caller_pcb->fd_arr[fd] == NULL){
+        printf("file doesn't exist at index: %d\n", fd);
+        return 0;
+    }
+    if (fd >= 0 && fd < 8 && caller_pcb->fd_arr[fd]->flags == 1){
+        int32_t ret = caller_pcb->fd_arr[fd]->file_op_table_pointer->read(file_array[fd], buf, nbytes);
+        printf("it exists %d\n", ret);
+        return ret;
+    }
+
     return 0; // returns 0 if fail - initial file position is at or beyond EOF for normal files
 }
 int32_t write (int32_t fd, const void* buf, int32_t nbytes){
     // returns number of bytes written
     printf("write systemcall called");
     // TODO: get current pcb
-    // if (fd >= 0 && fd < 8 && file_array[fd].flags == 1 && buf == NULL){
-    //     int32_t ret = file_array[fd]->file_op_table_pointer->write(file_array[fd],buf,nbytes);
-    //     return ret;
-    // }
+    pcb_t * caller_pcb;
+    caller_pcb = get_last_pcb();
+    if (fd >= 0 && fd < 8 && caller_pcb->fd_arr[fd]->flags == 1 && buf == NULL){
+        int32_t ret = caller_pcb->fd_arr[fd]->file_op_table_pointer->write(file_array[fd],buf,nbytes);
+        return ret;
+    }
+
     return -1; // returns -1 on failure
 }
 int32_t open (const uint8_t* filename){
@@ -173,54 +183,54 @@ int32_t open (const uint8_t* filename){
         return -1; // check whether read_dentry worked
     }
 
-    // int i;
-    // for (i = 2; i < FD_ARRAY_SIZE; i++){
-    //     // check for unused idx
-    //     if (file_array[i] == NULL){
-    //         // put fd in here
-    //         fd_t new_fd;
-    //         new_fd.flags = 1;
-    //         int sec_check;
-    //         if (dentry.file_type == 0){
-    //             // rtc
-    //             new_fd.inode = NULL;
-    //             new_fd.file_pos = 0;
-    //             new_fd.file_op_table_pointer = &rtc_jump;
-    //             // call open
-    //             sec_check = rtc_jump.open(&new_fd, filename);
-    //         }
-    //         else if ( dentry.file_type == 1){
-    //             // directory
-    //             new_fd.inode = dentry.inode_num;
-    //             new_fd.file_pos = 0;
-    //             new_fd.file_op_table_pointer = &dir_jump;
-    //             // call open
-    //             sec_check = dir_jump.open(&new_fd, filename);
-    //         }
-    //         else if ( dentry.file_type == 2){
-    //             // file
-    //             new_fd.inode = dentry.inode_num;
-    //             new_fd.file_pos = 0;
-    //             new_fd.file_op_table_pointer = &file_jump;
-    //             // call open
-    //             sec_check = file_jump.open(&new_fd, filename);
-    //         }
-    //         else {
-    //             printf("opening invalid filetype");
-    //             return -1;
-    //         }
-    //         if (sec_check == -1) return -1; // check if opened failed
-    //         //TODO: add new_fd to the pcb
-    //         return i;
-    //     }
-    //     // use pcb.filearray later
-    //     else if (file_array[i].inode == dentry.inode_num){
-    //         // file is already opened
-    //         printf("file is already opened");
-    //         return -1;
-    //     }
+ int i;
+ for (i = 2; i < FD_ARRAY_SIZE; i++){
+     // check for unused idx
+     if (caller_pcb->fd_arr[i] == NULL){
+         // put fd in here
+         fd_t new_fd;
+         new_fd.flags = 1;
+         int sec_check;
+         if (dentry.file_type == 0){
+             // rtc
+             new_fd.inode = NULL;
+             new_fd.file_pos = 0;
+             new_fd.file_op_table_pointer = &rtc_jump;
+             // call open
+             sec_check = rtc_jump.open(&new_fd, filename);
+         }
+         else if ( dentry.file_type == 1){
+             // directory
+             new_fd.inode = dentry.inode_num;
+             new_fd.file_pos = 0;
+             new_fd.file_op_table_pointer = &dir_jump;
+             // call open
+             sec_check = dir_jump.open(&new_fd, filename);
+         }
+         else if ( dentry.file_type == 2){
+             // file
+             new_fd.inode = dentry.inode_num;
+             new_fd.file_pos = 0;
+             new_fd.file_op_table_pointer = &file_jump;
+             // call open
+             sec_check = file_jump.open(&new_fd, filename);
+         }
+         else {
+             printf("opening invalid filetype");
+             return -1;
+         }
+         if (sec_check == -1) return -1; // check if opened failed
+         caller_pcb->fd_arr[i] = &new_fd; // add fd_t to file array
+         return i;
+     }
+     // use pcb.filearray later
+     else if (caller_pcb->fd_arr[i]->inode == dentry.inode_num){
+         // file is already opened
+         printf("file is already opened");
+         return -1;
+     }
 
-    // }
+    }
     return -1; // returns -1 on failure
 }
 int32_t close (int32_t fd){
