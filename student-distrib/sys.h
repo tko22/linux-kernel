d369fb395d9cd3c969bd59bcd9c5c92be2cd48d3
Hubert Deng <hubertdeng123@gmail.com>
2018-04-08 20:01:47 -0500

    updated address

--- student-distrib/sys.h
+++ student-distrib/sys.h
@@ -10,9 +10,9 @@
 #define USER_ADDRESS 0x8048000  //virtual address
 #define OFFSET_ADDR 0x48000
 #define FOUR_KB 4096 //4KB
-#define FOUR_MB 0x40000 //4 MB
+#define FOUR_MB 0x400000 //4 MB
 #define KERNEL_STACK_SIZE 0x2000 // 8KB
-#define MB8_ADDRESS 0x80000 // 8 MB
+#define MB8_ADDRESS 0x800000 // 8 MB
 #define KB8 0x2000 // 8 KB
 #define PCB_MASK 0xFFFFE000
 #define MAGIC_EXECUTABLE1 0x7f
