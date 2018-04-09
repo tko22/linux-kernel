423e95251d15fe4478beb7bc947d7d5ba20917ea
Timothy Ko <tk2@illinois.edu>
2018-04-08 21:43:13 -0500

    shit

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -10,11 +10,11 @@ extern unsigned char getScanCode();
 extern unsigned char getChar(unsigned char character);
 extern void handle_keyboard_interrupt();
 void update_cursor(int row, int col);
-int32_t terminal_open(const uint8_t* filename);
-int32_t terminal_close(int32_t fd);
-int32_t terminal_write(int32_t fd, char *string, int32_t length);
-int32_t terminal_read(int32_t fd, char *string, int32_t length);
-int32_t invalid_func();
+extern int32_t terminal_open(const uint8_t* filename);
+extern int32_t terminal_close(int32_t fd);
+extern int32_t terminal_write(int32_t fd, char *string, int32_t length);
+extern int32_t terminal_read(int32_t fd, char *string, int32_t length);
+extern int32_t invalid_func();
 void update_boundaries();
 void enable_cursor();
 
