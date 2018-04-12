7d863e7888aed7a132944eaeead3604a5e06e933
Shubhit <dharnid2@illinois.edu>
2018-04-12 13:34:44 -0500

    IDT TrapGate Fixed

--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -7,7 +7,7 @@
 extern void init_rtc();
 extern void handle_rtc_interrupt();
 
-extern int32_t open_rtc(const uint8_t* filename);
+extern int32_t open_rtc(fd_t* fd, const uint8_t* filename);
 extern int32_t close_rtc(fd_t* fd);
 extern int32_t write_rtc(fd_t* fd, const uint8_t* buf, int32_t nbytes);
 extern int32_t read_rtc(fd_t* fd, uint8_t* buf, int32_t nbytes);
