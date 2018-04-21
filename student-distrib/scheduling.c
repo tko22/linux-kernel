3e4f8a11b456d2bb460b8d13d52dbf490df21a7b
Timothy Ko <timothy.l.ko@gmail.com>
2018-04-21 15:52:10 -0500

    switch terminal

--- student-distrib/scheduling.c
+++ student-distrib/scheduling.c
@@ -6,7 +6,7 @@
 #include "lib.h"
 
 volatile int process;
-
+#define TERM_VID_BUFF 0x40000000
 void initalize_PIT(){
     enable_irq(0); //IRQ 0 is for PIT
     //TODO set the frequency for ticks to use in Round-Robin Method
@@ -62,15 +62,30 @@ void switch_proc(){
     pcb_t * caller_pcb;
     caller_pcb=get_last_pcb();
 }
-void init_terminal_buf(uint32_t terminal_id){
+void init_terminal_buf(){
     int i;
     for(i = 0; i < PAGE_TABLE_SIZE; i++){
       terminal_page_table[i] = EMPTY_ENTRY;
     }
     page_directory[16] = (uint32_t)terminal_page_table | ENABLE_ENTRY;
-    terminal_page_table[0] = (uint)
+    terminal_page_table[0] = (uint32_t)TERM_VID_BUFF | ENABLE_ENTRY;
+    terminal_page_table[1] = (uint32_t)(TERM_VID_BUFF + _4KB) | ENABLE_ENTRY;
+    terminal_page_table[2] = (uint32_t)(TERM_VID_BUFF + _4KB) | ENABLE_ENTRY;
 }
 // terminal id is from 0-2
 void switch_terminal(uint32_t terminal_id){
-    active_terminal = terminal_id;
+    // save real video buffer
+    memcpy((TERM_VID_BUFF) + (currenterminal)*_4KB, VIDEO_ADDR, _4KB );
+    // change the current terminal to new terminal - global variable
+    currenterminal = terminal_id;
+
+    // copy other terminal buffer to new terminal
+    memcpy(VIDEO_ADDR, (TERM_VID_BUFF) + (terminal_id)*_4KB, _4KB );
+
+
+    // remember about vidmap!!!
+    // TODO: Switch video paging
+    // TODO: Switch Input keyboard buffer
+    // TODO: Update visible video coordinates
+
 }
