914bc16da4a9cc202bbbdb2bdbaa859f85211ea7
Tae <t.lawphong@gmail.com>
2018-04-28 12:02:10 -0500

    trying to fix the 8KB gap (AGAIN)

--- student-distrib/sys.c
+++ student-distrib/sys.c
@@ -139,7 +139,7 @@ int32_t execute(const uint8_t* command){
         }
     }
     pcb_t *p_address;
-    if((uint32_t*)get_last_pcb() == (uint32_t*)0x7F000){ // if it's the first process
+    if((uint32_t*)get_last_pcb() == (uint32_t*)0x7FE000){ // if it's the first process
       p_address = (pcb_t*)((uint32_t)get_last_pcb());
     }
     else{
